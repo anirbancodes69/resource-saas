5e18532f1602ef0fbca43e3271eef80e00eb8746
anirbancodes69 <anirbancodes@gmail.com>
2026-02-25 04:53:47 +0530

    fix(auth): remove guard factory dependency

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -30,7 +30,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60,
+            'expires_in' => config('jwt.ttl') * 60,
         ], 201);
     }
 }
